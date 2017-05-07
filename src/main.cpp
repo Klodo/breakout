98d9185780abafbe42c991460fe214cd6662d1f9
Kevin Salvesen <kevin@kevin-korora25.fritz.box>
2017-05-07 23:27:13 +0200

    add rendering of bricks/paddle and rudimentary paddle movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,48 @@
 #include <SDL2/SDL.h>
 
 #include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <thread>
+#include <vector>
+
+/*************/
+/*** types ***/
+/*************/
+struct Color {
+    std::uint8_t r = 0;
+    std::uint8_t g = 0;
+    std::uint8_t b = 0;
+    std::uint8_t a = 0;
+};
+
+struct Brick {
+    SDL_Rect rect;
+    int health = 0;
+
+    Brick() = default;
+    Brick(int x, int y, int w, int h, int start_health)
+        : rect({x, y, w, h}), health(start_health) {}
+};
+
+struct Paddle {
+    SDL_Rect rect;
+
+    Paddle() = default;
+    Paddle(int x, int y, int w, int h) : rect({x, y, w, h}) {}
+};
 
 /*****************************/
 /*** function declarations ***/
 /*****************************/
+bool init_sdl();
+void clean_sdl();
+void init_game();
 void handle_events();
 void handle_keydown(const SDL_KeyboardEvent& event);
+void handle_mouse_button_down(const SDL_MouseButtonEvent& event);
 void render();
+void render_rect(const SDL_Rect& rect, Color color);
 
 /*****************/
 /*** constants ***/
@@ -17,22 +50,61 @@ void render();
 constexpr int WINDOW_WIDTH = 640;
 constexpr int WINDOW_HEIGHT = 480;
 
+constexpr int NUM_BRICK_COLUMNS = 13;
+constexpr int NUM_BRICK_ROWS = 10;
+
+constexpr Color BRICK_COLORS[NUM_BRICK_ROWS] = {
+    {200, 200, 200, 255},
+    {180, 180, 180, 255},
+    {160, 160, 160, 255},
+    {140, 140, 140, 255},
+    {120, 120, 120, 255},
+    {100, 100, 100, 255},
+    {80, 80, 80, 255},
+    {60, 60, 60, 255},
+    {40, 40, 40, 255},
+    {20, 20, 20, 255},
+};
+
+constexpr int BRICK_WIDTH = 40;
+constexpr int BRICK_HEIGHT = 20;
+
+constexpr Color PADDLE_COLOR = {100, 0, 40, 255};
+constexpr int PADDLE_WIDTH = 100;
+constexpr int PADDLE_HEIGHT = 20;
+constexpr int PADDLE_SPEED = 3;
+
 /***************/
 /*** globals ***/
 /***************/
 bool g_is_running = true;
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
+std::vector<Brick> g_bricks;
+Paddle g_paddle;
 
 /****************************/
 /*** function definitions ***/
 /****************************/
 int main(int argc, char** argv) {
-    std::cout << "breakout\n";
+    if (init_sdl() == false) {
+        return EXIT_FAILURE;
+    }
 
+    init_game();
+
+    while (g_is_running) {
+        handle_events();
+        render();
+    }
+
+    clean_sdl();
+}
+
+bool init_sdl() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "SDL_Init(SDL_INIT_EVERYTHING) != 0\n";
-        return EXIT_FAILURE;
+        return false;
     }
 
     g_window = SDL_CreateWindow("Breakout",
@@ -44,7 +116,7 @@ int main(int argc, char** argv) {
 
     if (g_window == nullptr) {
         std::cout << "g_window == nullptr\n";
-        return EXIT_FAILURE;
+        return false;
     }
 
     g_renderer = SDL_CreateRenderer(
@@ -52,16 +124,38 @@ int main(int argc, char** argv) {
 
     if (g_renderer == nullptr) {
         std::cout << "g_renderer == nullptr\n";
-        return EXIT_FAILURE;
+        return false;
     }
 
-    while (g_is_running) {
-        handle_events();
-    }
+    return true;
+}
 
+void clean_sdl() {
     SDL_DestroyRenderer(g_renderer);
     SDL_DestroyWindow(g_window);
     SDL_Quit();
+}
+
+void init_game() {
+    // init bricks
+    int offset_x = (WINDOW_WIDTH - (NUM_BRICK_COLUMNS * (BRICK_WIDTH + 1))) / 2;
+    int offset_y = 20;
+
+    for (int row = 0; row < NUM_BRICK_ROWS; ++row) {
+        for (int col = 0; col < NUM_BRICK_COLUMNS; ++col) {
+            g_bricks.emplace_back(offset_x + col * (BRICK_WIDTH + 1),
+                                  offset_y + row * (BRICK_HEIGHT + 1),
+                                  BRICK_WIDTH,
+                                  BRICK_HEIGHT,
+                                  NUM_BRICK_ROWS - row);
+        }
+    }
+
+    // init paddle
+    g_paddle = Paddle((WINDOW_WIDTH / 2) - (PADDLE_WIDTH / 2),
+                      WINDOW_HEIGHT - 30 - PADDLE_HEIGHT,
+                      PADDLE_WIDTH,
+                      PADDLE_HEIGHT);
 }
 
 void handle_events() {
@@ -73,7 +167,7 @@ void handle_events() {
             break;
         }
         case SDL_MOUSEBUTTONDOWN: {
-            // handle_mouse_button_down(event.button);
+            handle_mouse_button_down(event.button);
             break;
         }
         case SDL_QUIT: {
@@ -86,8 +180,16 @@ void handle_events() {
 }
 
 void handle_keydown(const SDL_KeyboardEvent& event) {
-    switch (event.keysym.sym) {
-    case SDLK_ESCAPE: {
+    switch (event.keysym.scancode) {
+    case SDL_SCANCODE_LEFT: {
+        g_paddle.rect.x -= PADDLE_SPEED;
+        break;
+    }
+    case SDL_SCANCODE_RIGHT: {
+        g_paddle.rect.x += PADDLE_SPEED;
+        break;
+    }
+    case SDL_SCANCODE_ESCAPE: {
         g_is_running = false;
         break;
     }
@@ -95,6 +197,26 @@ void handle_keydown(const SDL_KeyboardEvent& event) {
     }
 }
 
-void render() {
+void handle_mouse_button_down(const SDL_MouseButtonEvent& event) {
+    std::cout << '(' << event.x << ", " << event.y << ")\n";
+}
 
+void render() {
+    SDL_SetRenderDrawColor(g_renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(g_renderer);
+
+    // render bricks
+    for (const auto& brick : g_bricks) {
+        render_rect(brick.rect, BRICK_COLORS[brick.health - 1]);
+    }
+
+    // render paddle
+    render_rect(g_paddle.rect, PADDLE_COLOR);
+
+    SDL_RenderPresent(g_renderer);
+}
+
+void render_rect(const SDL_Rect& rect, Color color) {
+    SDL_SetRenderDrawColor(g_renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderFillRect(g_renderer, &rect);
 }
